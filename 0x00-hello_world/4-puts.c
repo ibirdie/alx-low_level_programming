ecc2f5ed9897ded19ea0219b286482f04162b124
ibirdie <bushbirdman@gmail.com>
2023-07-13 06:39:11 -0700

    alx-low_level_programming

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,18 @@
+/*
+ * 4.Hello, puts
+ * A new way to print
+ */
+
+#include <stdio.h>
+
+/**
+ * main : Print "Programming is like building a multilingual puzzle, followed by a new line.
+ *
+ * return :Always 0.
+ */
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+
+	return (0);
+}

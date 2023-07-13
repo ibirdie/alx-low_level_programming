4eaf5f45f7429c20a8c47ec0a9e1a384ba8dad04
ibirdie <bushbirdman@gmail.com>
2023-07-13 07:19:59 -0700

    alx-low_level_programming

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -4,7 +4,7 @@
  */
 #include <stdio.h>
 /**
- * main-prints "with proper grammar, but the outcome is a piece of art,"
+ * main-prints "with proper grammar, but the outcome is a piece of art, "
  *
  * Return: Always 0.
  */

7275ebe7756979f6bb4c82cb920578d8a1213c26
ibirdie <bushbirdman@gmail.com>
2023-07-15 08:29:56 -0700

    0x01-variables_if_else_while

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+  *main - 5-print_numbers.c
+  *
+  *Return: Always (0);
+  */
+
+int main(void)
+{
+	int num;
+    num = -1;
+	while (num != 10)
+	{
+		printf("%d", num, num++);
+
+	}
+
+	return (0);
+}

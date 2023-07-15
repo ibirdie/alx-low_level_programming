457170dfe0a87a04415afe6aa96cd6626e6513ce
ibirdie <bushbirdman@gmail.com>
2023-07-15 08:45:32 -0700

    0x01-variables_if_else_while

--- 0x01-variables_if_else_while/5-print_numbers.c
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -1,20 +1,11 @@
 #include <stdio.h>
 /**
-  *main - 5-print_numbers.c
-  *
-  *Return: Always (0);
-  */
-
+ *main-Function begins
+ *
+ *Return: function ends
+ */
 int main(void)
 {
-	int num = 0;
-
-	while (num != 10)
-	{
-	printf("%d ", num);
-	num = num + 1;
-	}
-
-
+	printf("0123456789\n");
 	return (0);
 }

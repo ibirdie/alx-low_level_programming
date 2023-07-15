001d381c83d5bec82e3e263ea6dc2b5c6fdead7b
ibirdie <bushbirdman@gmail.com>
2023-07-15 08:00:18 -0700

    0x01-variables_if_else_while

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ *main-function begins
+ *
+ *Return: function ends
+ */
+int main(void)
+{
+	char alph[25] = "abcdfghijklmnoprstuvwxyz\n";
+    int i = 0;
+    
+	while (i < 25 )
+	{
+		putchar(alph[i]);
+		i++;
+	}
+	return (0);
+}

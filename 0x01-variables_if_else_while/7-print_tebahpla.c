549acf389bcdc7a676986fa683eefd1d3dbcebcd
ibirdie <bushbirdman@gmail.com>
2023-07-15 16:57:38 -0700

    0x01-variables_if_else_while

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+/**
+  *main - 7-print_tebahpla.c
+  *
+  *Return: Always (0);
+  */
+
+int main(void)
+{
+
+
+	char a = 122;
+
+	while (a >= 97)
+	{
+		putchar(a);
+		a--;
+		putchar('\n');
+	}
+	return (0);
+}

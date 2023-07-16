cfd71a3dcc99728b329d9c0db284573a7baa2ba5
ibirdie <bushbirdman@gmail.com>
2023-07-16 08:26:26 -0700

    0x01-variables_if_else_while

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -1,26 +1,25 @@
 #include <stdio.h>
 /**
  * main - Advanced 100
- * 
+ *
  * Return: Always (0);
  */
- 
- int main (void)
- {
-     int a;
-     int b;
-     
-     for (a = 0; a < 9; a++)
-     {
-     	for (b = a + 1; b < 10; b++)
-      	{
-        	putchar(a + '0');
-            putchar(b + '0');
-            
-            if ( a == 8 && b == 9);
-            putchar(',');
-            putchar(' ');
-		}
-      }
-      return (0);
- }
+
+int main(void)
+{
+	int a;
+	int b;
+
+	for (a = 0; a < 9; a++)
+	{
+	for (b = a + 1; b < 10; b++)
+	{
+		putchar(a + '0');
+		putchar(b + '0');
+	if (a == 8 && b == 9)
+		putchar(',');
+		putchar(' ');
+	}
+	}
+return (0);
+}

55e4a864da20b382360c39930667bddf30ce7597
ibirdie <bushbirdman@gmail.com>
2023-07-15 17:17:04 -0700

    0x01-variables_if_else_while

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+/**
+ *main - 8-print_base16.c
+ *
+ * Return: Always (0)
+ */
+
+int main(void)
+{
+
+	char a = 97;
+	int b = 0;
+
+	while (b < 10)
+	{
+		putchar(b + '0');
+		b++;
+	}
+	while (a < 102)
+	{
+		putchar(a);
+		a++;
+	}
+		putchar('\n');
+	return (0);
+}

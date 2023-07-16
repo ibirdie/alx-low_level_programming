beabfe9a9b6ae2081d7e8ada4ff7a1b6cf6cbc14
ibirdie <bushbirdman@gmail.com>
2023-07-16 08:09:14 -0700

    0x01-variables_if_else_while

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -14,8 +14,8 @@
      {
      	for (b = a + 1; b < 10; b++)
       	{
-        	putchar(a%10 + '0');
-            putchar(b%10 + '0');
+        	putchar(a + '0');
+            putchar(b + '0');
             
             if ( a == 8 && b == 9);
             putchar(',');

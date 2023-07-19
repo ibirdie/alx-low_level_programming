e002b842c00a1f39323ea97e35f649bd77f3dadf
ibirdie <bushbirdman@gmail.com>
2023-07-18 17:44:11 -0700

    Herh

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,28 @@
+#include "main.h"
+/**
+  *print_sign - Number 5 ei.
+  *@n: Char representative.
+  *
+  *Return: 1, 0, -1.
+  */
+
+int print_sign(int n)
+{
+
+	if (n > 0)
+	{
+		_putchar(43);
+		return (1);
+	}
+	else if (n > 0)
+	{
+		_putchar (45);
+		return (-1);
+	}
+	else
+	{
+		_putchar(48);
+		return (0);
+	}
+}
+

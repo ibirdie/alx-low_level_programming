bc3b3aab2fcb54ed7363ad02905fdba9a583d8ec
ibirdie <bushbirdman@gmail.com>
2023-07-19 16:57:48 -0700

    ko

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -1,95 +1,44 @@
 #include "main.h"
 
 /**
- *print_times_table - Prints the times table of the input,
+ * print_times_table - Prints the times table of the input,
  *                     starting with 0.
  * @n: The value of the times table to be printed.
  */
-
- void print_times_table(int n)
+void print_times_table(int n)
 {
-	if (n < 0 || n > 15)
-		return;
-	int row, col, first_num, second_num, product;
-	for (row = 0; row <= n; row++)
+	int num, mult, prod;
+
+	if (n >= 0 && n <= 15)
 	{
-		for (col = 0; col <= n; col++)
+		for (num = 0; num <= n; num++)
 		{
-			product = row * col;
+			_putchar('0');
 
-			if (col == 0)
+			for (mult = 1; mult <= n; mult++)
 			{
-				putchar(product + '0');
-			}
+				_putchar(',');
+				_putchar(' ');
 
-			else if (product <= 9)
-			{
-				putchar(' ');
-				putchar(' ');
-				putchar(product + '0');
-			}
+				prod = num * mult;
 
-			else if (product > 9 && product < 100)
-			{
-				putchar(' ');
-				putchar(product / 10 + '0');
-				putchar(product % 10 + '0');
-			}
-			// else
-			// {
-			// 	if (product >= 100)
-			// 		putchar((product / 100) + '0');
-			// 	if (product >= 10)
-			// 		putchar(((product / 10) % 10) + '0');
-			// 	putchar(product % 10 + '0');
-			// }
-			else if (product >= 100)
-			{
-				putchar((product / 100) + '0');
-				putchar(((product / 10) % 10) + '0');
-				putchar(product % 10 + '0');
-			}
+				if (prod <= 99)
+					_putchar(' ');
+				if (prod <= 9)
+					_putchar(' ');
 
-			if (col != n)
-			{
-				putchar(',');
-				putchar(' ');
+				if (prod >= 100)
+				{
+					_putchar((prod / 100) + '0');
+					_putchar(((prod / 10)) % 10 + '0');
+				}
+				else if (prod <= 99 && prod >= 10)
+				{
+					_putchar((prod / 10) + '0');
+				}
+				_putchar((prod % 10) + '0');
 			}
+			_putchar('\n');
 		}
-		putchar('\n');
-	}
-}
-void print_times_tables(int n)
-{
-	int row, column, product;
-
-	if (n < 0 || n > 15)
-		return;
-
-	for (row = 0; row <= n; row++)
-	{
-		for (column = 0; column <= n; column++)
-		{
-			product = row * column;
-
-			if (column != 0)
-			{
-				putchar(',');
-				putchar(' ');
-
-				if (product < 100)
-					putchar(' ');
-				if (product < 10)
-					putchar(' ');
-			}
-
-			if (product >= 100)
-				putchar((product / 100) + '0');
-			if (product >= 10)
-				putchar(((product / 10) % 10) + '0');
-
-			putchar((product % 10) + '0');
-		}
-		putchar('\n');
 	}
 }

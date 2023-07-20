02ba6d254f65c0a59bcd5d68ab6e14e90397e502
ibirdie <bushbirdman@gmail.com>
2023-07-20 06:43:37 -0700

    commit de3 bebree

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -12,15 +12,14 @@ void print_most_numbers(void)
 int a;
 	for (a = 0; a <= 9; a++)
 	{
-		if  a == 2 || a == 4)
+		if (a == 2 || a == 4)
 		{
 			continue;
 		}
 
-	_putchar (a);
+	_putchar (a + '0');
 	}
 
 	_putchar ('\n');
 
-return (0);
 }

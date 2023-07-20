3e0c0b8aa725fecb8c625642e2c85d8b9cd602dd
ibirdie <bushbirdman@gmail.com>
2023-07-19 17:19:36 -0700

    mon

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -3,40 +3,42 @@
 /**
  * print_times_table - Prints the times table of the input,
  *                     starting with 0.
- * @n: The value of the times table to be printed
+ * @n: The value of the times table to be printed.
  */
-
-void print_times_tables(int n)
+void print_times_table(int n)
 {
-	int row, column, product;
+	int num, mult, prod;
 
-	if (n < 0 || n > 15)
-		return;
-
-	for (row = 0; row <= n; row++)
+	if (n >= 0 && n <= 15)
 	{
-		for (column = 0; column <= n; column++)
+		for (num = 0; num <= n; num++)
 		{
-			product = row * column;
+			_putchar('0');
 
-			if (column != 0)
+			for (mult = 1; mult <= n; mult++)
 			{
-				putchar(',');
-				putchar(' ');
+				_putchar(',');
+				_putchar(' ');
 
-				if (product < 100)
-					putchar(' ');
-				if (product < 10)
-					putchar(' ');
+				prod = num * mult;
+
+				if (prod <= 99)
+					_putchar(' ');
+				if (prod <= 9)
+					_putchar(' ');
+
+				if (prod >= 100)
+				{
+					_putchar((prod / 100) + '0');
+					_putchar(((prod / 10)) % 10 + '0');
+				}
+				else if (prod <= 99 && prod >= 10)
+				{
+					_putchar((prod / 10) + '0');
+				}
+				_putchar((prod % 10) + '0');
 			}
-
-			if (product >= 100)
-				putchar((product / 100) + '0');
-			if (product >= 10)
-				putchar(((product / 10) % 10) + '0');
-
-			putchar((product % 10) + '0');
+			_putchar('\n');
 		}
-		putchar('\n');
 	}
 }

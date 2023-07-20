db23e75883627235788bac468643e1f3fa7ef36d
ibirdie <bushbirdman@gmail.com>
2023-07-20 05:02:59 -0700

    two

--- 0x04-more_functions_nested_loops/1-isdigit.c
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -1,14 +1,14 @@
 #include "main.h"
 
 /**
-  *_isupper - Checking for uppercase.
+  *_isdigit - Checking for digits.
   *
   *Return; 1 or 0 if not.
   */
 
 int _isupper(int c)
 {
-	if (c = 'A' && c < 'Z')
+	if (c = '0' && c < '10')
 		return (1);
 	else
 		return (0);
---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,11 @@
+#include "main.h"
+/**
+  *mul - to multiply two integers
+  *
+  *Return; Always (0);
+  */
+
+int mul(int a, int b)
+{
+	int mul =(int a * int b);
+	return (0);

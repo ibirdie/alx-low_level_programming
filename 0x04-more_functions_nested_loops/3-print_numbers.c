ba17630646cbc25f8b3c331fb852fedeec01fa6a
ibirdie <bushbirdman@gmail.com>
2023-07-20 06:05:29 -0700

    boy

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+  *print_numbers - Printing 0 - 9.
+  *
+  *
+  *Return: Always 0.
+  */
+
+void print_numbers(void)
+{
+int a = ('0');
+	while (a <= '9')
+	{
+	_putchar (a);
+	a++;
+	}
+	_putchar ('\n');
+
+}
--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -1,4 +1,5 @@
 int _isupper(int c);
+void print_numbers(void);
 int mul(int a, int b);
 int _putchar(char c);
 int _isdigit(int c);

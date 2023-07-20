5c1fc402e5a7fbb86ec58cd44c087135f39edc32
ibirdie <bushbirdman@gmail.com>
2023-07-20 12:12:09 -0700

    test 7

---
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+  *print_diagonal - printing n number of '\'
+  *@n: input number
+  *
+  */
+
+void print_diagonal(int n)
+{
+	int a;
+		for (a = 1; a <= n; a++)
+		{
+			if (n > 0)
+				_putchar ('\\');
+		}
+		_putchar('\n');
+}
